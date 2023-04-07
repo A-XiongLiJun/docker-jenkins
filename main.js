f6ba2c36d7ab610796db05a3cf0aa7935fe2d89c
熊丽君 <664953382@qq.com>
2023-04-07 10:04:30 +0800

    up

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
-      <span>这是github仓库 服务器测试打包0.0.6</span>
+      <span>这是aliyun仓库 服务器测试打包0.0.0</span>
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
